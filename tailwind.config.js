73137262a68bbe50698d3bcbb616fc1d3a9f0646
Habeeb <habeebahmadu1@gmail.com>
2023-06-01 23:55:22 +0100

    feat: setup tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        light: "#F3FCF0",
+        dark: "#272727",
+        btn: "#1985A1",
+        icon: "#A71D31",
+        modal: "#1F271B",
+      },
+      animation: {
+        slide: "slide 25s linear infinite",
+      },
+      keyframes: {
+        slide: {
+          "0%,100%": { transform: "translateX(5%)" },
+          "50%": { transform: "translateX(-125%)" },
+        },
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("tailwind-scrollbar"), require("tailwind-scrollbar-hide")],
+};
